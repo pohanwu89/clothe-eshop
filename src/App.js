31a3b6b68a40a3f95bd89e126dfa50b3d979cc75
Marek <jonas.wu@waystorm.com>
2019-09-27 20:55:08 +0900

    homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import HomePage from './pages/homepage/homepage'
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World lets update!</div>;
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+
+    )
+
   }
 }
-
 export default App;
